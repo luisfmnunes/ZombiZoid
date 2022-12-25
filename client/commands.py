0538b2c7ef38a216c598e68ca4a9e01e2eea337e
Luis Nunes <luisfelipe5417@gmail.com>
2022-12-25 20:22:25 +0000

    Update PZ Server commands dict

--- client/commands.py
+++ client/commands.py
@@ -8,9 +8,11 @@ from easydict import EasyDict as edict
 zz_commands = [
     edict({"name": "players", "args": [0]}),
     edict({"name": "save", "args": [0]}),
-    edict({"name": "startrain", "args": [0]}),
+    edict({"name": "startrain", "args": [0, 1]}),
     edict({"name": "stoprain", "args": [0]}),
     edict({"name": "showoptions", "args": [0]}),
+    edict({"name": "checkModsNeedUpdate", "args": [0]}),
     edict({"name": "servermsg", "args": [1]}),
-    edict({"name": "additem", "args": [2]}),
+    edict({"name": "teleport", "args": [2]}),
+    edict({"name": "additem", "args": [2, 3]}),
 ]
